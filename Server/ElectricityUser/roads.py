990dffde2032fa369ecfc262855a62638724213a
EoinMcD <EoinMcD@users.noreply.github.com>
2021-10-27 15:23:55 +0100

    First changes to infrastructure.py

--- Server/ElectricityUser/roads.py
+++ Server/ElectricityUser/roads.py
@@ -12,24 +12,44 @@
 #   Streetlights power output calculator is currently one hour, must make daily add up all hours in the night
 
 import random
-from street_lights import StreetLight 
-from traffic_lights import TrafficLight
 
 AVERAGE_ELECTRICITY_USAGE = 30
-
+STREET_LIGHT_USAGE = 0.08
+TRAFFIC_LIGHT_USAGE= random.randrange(90, 160, 1)/100
 
 class Road():
-    def __init__(self, roadID, totalElectricityUsage):
+    def __init__(self, roadID):
         self.roadID = roadID
-        self.totalElectricityUsage = totalElectricityUsage
-        self.hasTrafficLight = setTrafficLight()
-        self.hasStreetLight = setStreetLight()
+        self.hasTrafficLight = self.setTrafficLight
+        self.hasStreetLight = self.setStreetLight
+        self.sumElectricitityUsage
 
     def setRoadID(self, newID):
         self.roadID = newID
 
     def setTotalElectricityUsage(self, newValue):
         self.totalElectricityUsage = newValue
+
+    def setStreetLight():        
+        if random.randint(0, 1) == 0:
+            return True
+        else:
+            return False
+
+    def setTrafficLight():
+        if random.randint(0, 4) == 0:
+            return True
+        else:
+            return False
+
+    def sumElectricitityUsage(self):
+        electricityUsageTolerance = 10
+        dailyAverageUsage = random.randint(AVERAGE_ELECTRICITY_USAGE, AVERAGE_ELECTRICITY_USAGE + electricityUsageTolerance)/4 
+        if self.setTrafficLight() == True:
+            dailyAverageUsage += TRAFFIC_LIGHT_USAGE
+        #Will be reliant on time
+        if self.setStreetLight() == True:
+            dailyAverageUsage += STREET_LIGHT_USAGE
 
     def toString(self):
         return  "ID:" + self.roadID + "\t\t\tTotal Electricity Usage: " + str(self.totalElectricityUsage) + "kWh" + "\t\t\tStreetLight?: " + str(self.hasStreetLight) + "\t\t\tTrafficLight?: " + str(self.hasTrafficLight)
@@ -39,39 +59,17 @@ def generateRoadData(numberOfRoads):    # NOTE: Will be dependent on number of h
     roadCounter = 0
 
     for i in range(numberOfRoads):
-        road = Road("R" + str(roadCounter), sumElectricitityUsage(setStreetLight(), setTrafficLight()))
+        road = Road("R" + str(roadCounter))
         roadData.append(road)
         roadCounter += 1
     
     return roadData
 
-def sumElectricitityUsage(hasStreetLight,hasTrafficLight):
+def sumElectricitityUsage():
     electricityUsageTolerance = 10
     dailyAverageUsage = random.randint(AVERAGE_ELECTRICITY_USAGE, AVERAGE_ELECTRICITY_USAGE + electricityUsageTolerance)/4 
-    if(hasStreetLight == True):
-        sLight = StreetLight()
-        sLight.calcElectricityUsage()
-        dailyAverageUsage += sLight.totalElectricityUsage
-
-    if(hasTrafficLight == True):
-        tLight = TrafficLight()
-        tLight.calcElectricityUsage
-        dailyAverageUsage += tLight.totalElectricityUsage
-
     return dailyAverageUsage
-
-def setStreetLight():
-    if random.randint(0, 1) == 0:
-        return True
-    else:
-        return False
-
-def setTrafficLight():
-    if random.randint(0, 4) == 0:
-        return True
-    else:
-        return False
-        
+  
 roadArray = generateRoadData(10)
 for i in roadArray:
     print(i.toString())
