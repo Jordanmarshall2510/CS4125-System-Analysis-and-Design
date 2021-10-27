c611480659a61fb54bc34a957e1c2390a9fddf00
Jordan Marshall <47557144+Jordanmarshall2510@users.noreply.github.com>
2021-10-27 22:17:05 +0100

    Added JSON to solar and wind

--- Server/ElectricityGenerator/solar.py
+++ Server/ElectricityGenerator/solar.py
@@ -1,4 +1,17 @@
+import json
 from Server import ElectricityGenerator
+
+with open("config.json") as json_file:
+    conf = json.load(json_file)
+
+# Comment 1
+TEST_1= conf["electricityGenerator"]["solar"]["test1"]
+
+# Comment 2
+TEST_2 = conf["electricityGenerator"]["solar"]["test2"]
+
+# Comment 3
+TEST_3 = conf["electricityGenerator"]["solar"]["test3"]
 
 class Solar(ElectricityGenerator):
     def update(date):
--- Server/ElectricityGenerator/wind.py
+++ Server/ElectricityGenerator/wind.py
@@ -1,4 +1,17 @@
+import json
 from Server import ElectricityGenerator
+
+with open("config.json") as json_file:
+    conf = json.load(json_file)
+
+# Comment 1
+TEST_1= conf["electricityGenerator"]["wind"]["test1"]
+
+# Comment 2
+TEST_2 = conf["electricityGenerator"]["wind"]["test2"]
+
+# Comment 3
+TEST_3 = conf["electricityGenerator"]["wind"]["test3"]
 
 class Wind(ElectricityGenerator):
     def update(date):
