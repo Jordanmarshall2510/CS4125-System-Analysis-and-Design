be5efcce1ede9185ad3c95fefc39eb2153f2f26a
Jakub <18260179@studentmail.ul.ie>
2021-10-31 15:53:14 +0000

    fixed variables in solar and wind

--- Server/ElectricityGenerator/solar.py
+++ Server/ElectricityGenerator/solar.py
@@ -5,14 +5,13 @@ from Server import ElectricityGenerator
 from World import environment
 
 class Solar(ElectricityGenerator, clock):
-    wattage= conf["electricityGenerator"]["solar"]["output"]
+    wattage=0
 
     def __init__(self):
         path = os.path.dirname(os.path.realpath(__file__)).split("ElectricityGenerator")[0] + "config.json"
         with open(path) as json_file:
             conf = json.load(json_file)
-            self.delay=conf['world']['environment']['delay']
-            self.weather_change=conf['world']['environment']['change']
+            self.wattage=conf["electricityGenerator"]["solar"]["output"]
 
     def update(self):
         if(clock.getDayLight()==True):
