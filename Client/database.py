d88777c0120f3037f9eea129fd521127ae1b42a3
Marcin Sęk <marcin.j.sek@gmail.com>
2021-11-09 15:40:56 +0000

    Finished Database Clientside

--- Client/database.py
+++ Client/database.py
@@ -5,10 +5,21 @@ class Database:
 		self.con = sqlite3.connect("../Server/database.db")
 		self.cur = self.con.cursor()
 
-	def selectPowerHistory(self, type):
+	def selectUsedPowerHistory(self, type):
 		self.cur.execute("SELECT time, power_used FROM users WHERE type = ? ORDER BY time", [type])
 		return self.cur.fetchall()
+
+	def selectGeneratedPowerHistory(self, type):
+		self.cur.execute("SELECT time, power_used FROM generators WHERE type = ? ORDER BY time", [type])
+		return self.cur.fetchall()
 		
+	def selectTotalUsedPowerHistory(self):
+		self.cur.execute("SELECT time, SUM(power_used) AS total FROM users GROUP BY time")
+		return self.cur.fetchall()
+
+	def selectTotalGeneratedPowerHistory(self):
+		self.cur.execute("SELECT time, SUM(power_used) AS total FROM generators GROUP BY time")
+		return self.cur.fetchall()
 
 	def __del__(self):
 		self.con.close()
