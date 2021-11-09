347f85bd1f2be8edbd5c60e94ddbcc40148cb143
Marcin Sęk <marcin.j.sek@gmail.com>
2021-11-09 13:19:41 +0000

    Fixed datetime and completed the session file

--- Server/session.py
+++ Server/session.py
@@ -1,11 +1,12 @@
-# from datetime import timedelta, datetime
 import os
 import json
+from datetime import datetime, timedelta
 from ElectricityUser.businesses import Business
 from ElectricityUser.houses import House
 from ElectricityUser.infrastrucure import Infrastructure
 from ElectricityUser.vehicles import Vehicle
-# from ElectricityGenerator.distribution import Distribution
+from ElectricityGenerator.distribution import Distribution
+from databasemanager import insertUsage, insertGeneration
 
 path = os.path.dirname(os.path.realpath(__file__)) + "//config.json"
 
@@ -13,7 +14,7 @@ path = os.path.dirname(os.path.realpath(__file__)) + "//config.json"
 arrUsers = []
 
 # Set distribution
-# distribution = Distribution()
+distribution = Distribution()
 
 # Load users
 with open(path) as json_file:
@@ -25,29 +26,24 @@ with open(path) as json_file:
     arrUsers += Vehicle.generateUsers(conf["session"]['electricityUser']['vehicles'])
 
 # Initialise timer
-# timestamp = datetime.today()
-day = 0
+timestamp = datetime.now()
 while (True):
     # Create dictionary for the day
     dict = {}
 
-    # Progress time
-    # timestamp += datetime.timedelta(hours=1)
-
     # Update Distribution
-    # distribution.update(timestamp) # Might need a change
+    distribution.update(timestamp) # Might need a change
 
     # Update Users
     for user in arrUsers:
-        # distribution.update(100)
-        electricityUsed = user.update(100)
+        electricityUsed = user.update(timestamp)
         if type(user).__name__ in dict:
             dict[type(user).__name__] += electricityUsed
         else:
             dict[type(user).__name__] = electricityUsed
 
     print("=================================")
-    print(f"Day {day}")
+    print(f"Timestamp: {timestamp}")
     print("-- Users --")
     for key in dict:
         print(f"Total usage from {key}: {dict[key]}")
@@ -56,5 +52,8 @@ while (True):
     print("=================================")
 
     # Put data into database
+    insertUsage(timestamp, dict)
+    # insertGeneration(timestamp, dict) # Will this be a new dictionary ? (How to access solar and wind)
 
-    day += 1
+    # Progress time
+    timestamp += timedelta(hours=1)
