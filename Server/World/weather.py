3a55a42f43e2ac774d44ac7211ab0fbde8587d27
Jakub <18260179@studentmail.ul.ie>
2021-11-21 17:56:09 +0000

    fixed indentation

--- Server/World/weather.py
+++ Server/World/weather.py
@@ -55,36 +55,36 @@ class Weather:
     @staticmethod
     def get_season_change(date):
         if (Weather.climate.lower() == 'tropical'):
-        if not(3 <= int(date.strftime("%m")) <= 10):#dry season
-            return 'dry'
-        else:
-            return 'wet'
-        elif (Weather.climate.lower() == 'dry_cold' or 'dry_hot'):
-            if (Weather.climate.lower() == 'dry_cold'):
-                return 'winter'
+            if not(3 <= int(date.strftime("%m")) <= 10):#dry season
+                return 'dry'
             else:
-                return 'summer'
+                return 'wet'
+            elif (Weather.climate.lower() == 'dry_cold' or 'dry_hot'):
+                if (Weather.climate.lower() == 'dry_cold'):
+                    return 'winter'
+                else:
+                    return 'summer'
 
-        elif (Weather.climate.lower() == 'temperate'):
-            if 3 <= int(date.strftime("%m")) <= 6 :#spring
-                if 3 == int(date.strftime("%m")) and int(date.strftime("%d")) < 20:
+            elif (Weather.climate.lower() == 'temperate'):
+                if 3 <= int(date.strftime("%m")) <= 6 :#spring
+                    if 3 == int(date.strftime("%m")) and int(date.strftime("%d")) < 20:
+                        return 'winter'
+                    elif 6 == int(date.strftime("%m")) and int(date.strftime("%d")) > 20:
+                        return 'summer'
+                    else:
+                        return 'spring'
+                elif 6 <= int(date.strftime("%m")) <= 9 :#summer
+                    if 9 == int(date.strftime("%m")) and int(date.strftime("%d")) > 21:
+                        return 'autumn'
+                    else:
+                        return 'summer'
+                elif 9 <= int(date.strftime("%m")) <= 12 :#autumn
+                    if 12 == int(date.strftime("%m")) and int(date.strftime("%d")) > 20:
+                        return 'winter'
+                    else:
+                        return 'autumn'
+                else:#winter
                     return 'winter'
-                elif 6 == int(date.strftime("%m")) and int(date.strftime("%d")) > 20:
-                    return 'summer'
-                else:
-                    return 'spring'
-            elif 6 <= int(date.strftime("%m")) <= 9 :#summer
-                if 9 == int(date.strftime("%m")) and int(date.strftime("%d")) > 21:
-                    return 'autumn'
-                else:
-                    return 'summer'
-            elif 9 <= int(date.strftime("%m")) <= 12 :#autumn
-                if 12 == int(date.strftime("%m")) and int(date.strftime("%d")) > 20:
-                    return 'winter'
-                else:
-                    return 'autumn'
-            else:#winter
-                return 'winter'
 
         elif (Weather.climate.lower() == 'continental'):
             if 3 <= int(date.strftime("%m")) <= 6 :#spring
