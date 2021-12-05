29ad04c8dedf4551152f837c9826bce5fdb739d3
Crystal4B <marcin.j.sek@gmail.com>
2021-12-05 12:48:14 +0000

    Updating schema for remote use

--- database/schema.sql
+++ database/schema.sql
@@ -1,4 +1,17 @@
--- create
+-- Create database
+CREATE DATABASE smart_city;
+
+-- Create user for simulation
+CREATE USER "SimUser" IDENTIFIED BY "!Sim_Password21";
+
+-- Switch to database
+USE smart_city;
+
+-- Grant user correct permissions
+GRANT ALL PRIVILEGES ON smart_city TO "SimUser";
+FLUSH PRIVILEGES;
+
+-- create tables
 CREATE TABLE users (
 	id INT NOT NULL AUTO_INCREMENT,
 	user_type varchar(16) NOT NULL,
